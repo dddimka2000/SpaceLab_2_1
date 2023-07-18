5191a9d1495e0cdd2028fb4106424a708f1084ac
dddimka2000 <2000-dimk@ukr.net>
2023-07-18 10:04:35 +0300

    Тесты-фикс

--- src/test/java/org/example/service/MailSenderServiceTest.java
+++ src/test/java/org/example/service/MailSenderServiceTest.java
@@ -1,49 +1,14 @@
 package org.example.service;
 
-import com.icegreen.greenmail.util.GreenMail;
-import com.icegreen.greenmail.util.GreenMailUtil;
-import com.icegreen.greenmail.util.ServerSetup;
-import org.example.service.MailSenderService;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.ArgumentCaptor;
-import org.mockito.Captor;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
-import org.springframework.mail.javamail.MimeMessageHelper;
-
-import javax.mail.MessagingException;
-import javax.mail.internet.MimeMessage;
-
-import java.io.IOException;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.*;
 @SpringBootTest
 
 public class MailSenderServiceTest {
-    private GreenMail greenMail;
 
     @Autowired
     private MailSenderService mailSenderService;
-
-    @BeforeEach
-    public void setup() {
-        ServerSetup serverSetup = new ServerSetup(2525, "localhost", "smtp");
-        greenMail = new GreenMail(serverSetup);
-        greenMail.start();
-    }
-
-    @AfterEach
-    public void tearDown() {
-        greenMail.stop();
-    }
 
     @Test
     public void testSendSimpleMail() throws Exception {
